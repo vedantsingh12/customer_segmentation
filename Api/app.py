4781bc92eddec2dcfebc357091ef64c5f99d8f81
vedantsingh12 <vs3902182@gmail.com>
2024-12-03 18:58:54 +0530

    added segment output

--- Api/app.py
+++ Api/app.py
@@ -2,15 +2,15 @@ from fastapi import FastAPI
 import pandas as pd
 
 app=FastAPI()
+data=pd.read_csv(r'C:\Users\vs390\OneDrive\Desktop\customer_segmentation_projects\Data\customer_segment_data.csv')
 
-@app.get("test")
-def read_root():
-    return {'hello':'world'}
+total_customer=len(data)
+  
+segment_stats=data.groupby('segment').agg({
+    'Monthly_Revenue':'mean',
+    'CLTV':'mean',
+    'Age':'mean',
+    'Customer_ID':'count'
+}).rename(columns={'Customer_ID':'customer_count'}).round(2)
 
-@app.get("/")
-def read_root():
-    return {'hello':'world'}
 
-@ app.get('Segment')
-def segment ():
-    return (10)*(5)
